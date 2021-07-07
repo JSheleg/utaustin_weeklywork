21d1e47a98d0c6b12088ddc5d6af24960dbe5f57
JSheleg <jessica.sheleg@gmail.com>
2021-07-06 20:43:58 -0500

    solved challenge 2

--- week_10/10-OOP/01-Activities/02-Mini-Bank/Unsolved/minibank.js
+++ week_10/10-OOP/01-Activities/02-Mini-Bank/Unsolved/minibank.js
@@ -3,20 +3,31 @@
 
 function MiniBank(balance) {
   this.balance = balance;
+
   this.getBalance = function() {
     return this.balance;
   };
     // Add a `setBalance()` function
     // YOUR CODE HERE
     //
-
+  this.setBalance = function(value){
+    this.balance = value;
+  };
     // Add a `deposit()` function
     // YOUR CODE HERE
     //
-
+  this.deposit = function(value){
+    let newBalance = value + this.getBalance();
+    this.setBalance(newBalance);
+    console.log(`deposit`,value)
+  };
     // Add a `withdraw()` function
     // YOUR CODE HERE
-    //
+  this.withdraw = function(value){
+    let postWithdrawBalance = this.getBalance() - value;
+    this.setBalance(postWithdrawBalance);
+    console.log(`Withdraw`, value)
+  };
 
     this.printBalance = function() {
       console.log(`Balance: ${this.getBalance()}`);
@@ -27,9 +38,15 @@ function MiniBank(balance) {
 // PART 2
 
 // Create a new `bank` object
+let bank = new MiniBank(0);
+console.log(bank);
 
 // Print the `bank` balance
+bank.printBalance();
 
 // Deposit some money and then print the `bank` balance
-
+bank.deposit(50)
+bank.printBalance();
 // Withdraw some money and then print the `bank` balance
+bank.withdraw(20);
+bank.printBalance();
