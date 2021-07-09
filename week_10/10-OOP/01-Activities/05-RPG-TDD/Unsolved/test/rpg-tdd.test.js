79b4e35a98016870ca5f85027d588087a8a67680
JSheleg <jessica.sheleg@gmail.com>
2021-07-08 20:44:36 -0500

    activity 05 solved

--- week_10/10-OOP/01-Activities/05-RPG-TDD/Unsolved/test/rpg-tdd.test.js
+++ week_10/10-OOP/01-Activities/05-RPG-TDD/Unsolved/test/rpg-tdd.test.js
@@ -2,17 +2,39 @@ const Character = require('../rpg-tdd');
 
 test('creates a character object', () => {
   // Create a new character with a `name`, `strength`, and `hitpoints` values
-  // YOUR CODE HERE
-  //
-  // Create a test that checks if the `name` is a `string`
-  // YOUR CODE HERE
-  //
-  // Create a test that checks if the `strength` and `hitpoints` are `numbers`
-  // YOUR CODE HERE
-  //
-  // Create a test that checks if the `hitpoints` is greater than the `strength`
-  // YOUR CODE HERE
-  //
+  const character = new Character( 'boy', 10,50 )
+  expect(character.name).toBe('boy');
+  expect(character.strength).toBe(10);
+  expect(character.hitpoints).toBe(50);
+
+});
+
+// Create a test that checks if the `name` is a `string`
+test('cheks if the name is a string', () => {
+  const character = new Character( 'boy',10,50);
+  expect(character.name).toEqual(expect.any(String));
+});
+
+// Create a test that checks if the `strength` and `hitpoints` are `numbers`
+test('checks if the strength, and hitpoints are numbers', () => {
+  const character = new Character( 'boy',10,50);
+  expect(character.strength).toEqual(expect.any(Number));
+  expect(character.hitpoints).toEqual(expect.any(Number));
+});
+
+// Create a test that checks if the `hitpoints` is greater than the `strength`
+test('cheks if the hitpoints is greater than the strength', () => {
+  const character = new Character('boy', 10,50);
+  expect(character.hitpoints).toBeGreaterThan(character.strength);
 });
 
 // Bonus: write a test that will check if the character is alive or not
+test('checks if character is alive', () => {
+  const character =new Character('boy', 10,50);
+  expect(character.isAlive()).toBeTruthy();
+  character.hitpoints = 0;
+  expect(character.isAlive()).toBeFalsy();
+});
+
+
+
