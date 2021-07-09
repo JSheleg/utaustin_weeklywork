826ae967e3f1c7c3ecbbcc69bf04cef57b79ebd4
JSheleg <jessica.sheleg@gmail.com>
2021-07-08 19:33:11 -0500

    solved activity 3

--- week_10/10-OOP/01-Activities/03-RPG-Prototypes/Unsolved/rpg-prototypes.js
+++ week_10/10-OOP/01-Activities/03-RPG-Prototypes/Unsolved/rpg-prototypes.js
@@ -1,20 +1,46 @@
 // Create a constructor function called `Character` that takes in 3 arguments: `name`, `strength`, `hitpoints`
 // YOUR CODE HERE
 //
+function Character (name, strength, hitpoints){
+    this.name = name;
+    this.strength = strength;
+    this.hitpoints = hitpoints;
+};
 
 // Create a prototype method called `printStats()` which prints all of the stats for a character
 // YOUR CODE HERE
-//
+
+Character.prototype.printStats = function() {
+    console.table(
+       {
+        name: this.name,
+        strength: this.strength,
+        hitpoints: this.hitpoints
+       }  
+    )
+
+}
 
 // Create a prototype method called `isAlive()` which prints whether or not this character is alive
 // by determining whether their `hitpoints` are above or below zero
 // YOUR CODE HERE
-//
+
+Character.prototype.isAlive = function(){
+    if(this.hitpoints > 0) {
+        console.log('alive')
+        return true;
+        
+    }
+    console.log('dead');
+    return false;
+};
 
 // Create a prototype method called `attack()` which takes in a second character
 // and subtracts this character's `strength` from their `hitpoints`
 // YOUR CODE HERE
-//
+Character.prototype.attack = function(enemy){
+    enemy.hitpoints -= this.strength;
+}
 
 // =============================================================
 
