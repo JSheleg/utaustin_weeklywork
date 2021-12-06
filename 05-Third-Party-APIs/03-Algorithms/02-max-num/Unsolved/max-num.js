8dd86dde30978fa28da751bb47b08f2fe1572c06
JSheleg <jessica.sheleg@gmail.com>
2021-12-05 21:31:51 -0600

    solved alg max num

--- 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,14 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    let large=0;
+    // let slarg;
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]>large){
+            large = arr[i];
+            console.log(large);
+        }
+    }
+    return large;
+
+};
