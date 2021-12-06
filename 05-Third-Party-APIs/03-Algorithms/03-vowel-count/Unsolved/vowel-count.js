5d62365e437ce58ba113e58b377a701554c0ab2f
JSheleg <jessica.sheleg@gmail.com>
2021-12-05 21:47:54 -0600

    solved alg vowel count

--- 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,24 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    let count = 0;
+    
+    // const vowels = ['a','e','i','o','u'];
+    
+    for(let i = 0; i<str.length; i++){
+        var string = str[i].toLowerCase();
+        
+        if(string === 'a' ||string === 'e' ||string === 'i' ||string === 'o' ||string ==='u'){
+            
+            count++;
+        }
+        else{
+            console.log("not a vowel");
+        }
+  
+    }
+
+    return count;
+
+    
+};
