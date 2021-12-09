48ae3fbbf1c6bb5119ea1566c1a91cc6a02f0242
JSheleg <jessica.sheleg@gmail.com>
2021-12-09 15:11:45 -0600

    solved algo

--- 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
+++ 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,17 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    let sentence = str.split(" ");
+    console.log(sentence);
+    var result = [];
+    
+    for(let i = 0; i<sentence.length; i++){
+        var sent = sentence[i].split("");
+        console.log(sent);
+        sent[0] = sent[0].toUpperCase();
+        result.push(sent.join(""));
+        console.log(result);
+    }
+
+    return result.join(" ");
+};
