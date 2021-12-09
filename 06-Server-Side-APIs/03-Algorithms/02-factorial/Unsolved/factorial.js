cd48844683b409b15489919f8fea2860b7a3fc96
JSheleg <jessica.sheleg@gmail.com>
2021-12-09 14:54:33 -0600

    solved algorithm

--- 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,13 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+    let result =1;
+    //factorials never go below 1, so we take from the num value and reduce down to 1. 
+    //multiple result by the value of i until i becomes 1.
+    for(var i = num; i > 1; i--){
+        result = result * i;
+        console.log(result);
+
+    }
+    return result;
+};
